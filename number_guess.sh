78b96218f63988c3b58d1b74f0de8267bd36452e
Sarang Wadode <sarangwadode@gmail.com>
2022-10-01 10:26:56 +0530

    feat: conditions added on guess

--- number_guess.sh
+++ number_guess.sh
@@ -2,9 +2,6 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
-#random number
-random=$(( 1 + $RANDOM % 1000 ))
-
 #get username
 echo "Enter your username:"
 read USERNAME
@@ -24,8 +21,41 @@ then
   echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GUESS guesses."
 else
   #if u_name not present in db 
-  #insert user
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+
+  #insert to users table
+  INSERTED_TO_USERS=$($PSQL "insert into users(name) values('$USERNAME')")
   #get user_id
-  echo no user is not present
+  USER_ID=$($PSQL "select u_id from users where name = '$USERNAME'")
+  echo $USER_ID
+fi
+
+#secret number
+SECRET=$(( 1 + $RANDOM % 1000 ))
+
+#count guesses
+TRIES=0
+#guess number
+echo "Guess the secret number between 1 and 1000:"
+echo $SECRET
+read GUESS
+
+
+#track count of tries
+#if correct guess
+if [[ $SECRET = $GUESS ]]
+then
+  TRIES=$(( $TRIES + 1 ))
+  echo "You guessed it in $TRIES tries. The secret number was $SECRET. Nice job!"
+  #insert into db
+#if greater
+elif [[ $SECRET -gt $GUESS ]]
+then
+  TRIES=$(( $TRIES + 1 ))
+  echo "It's higher than that, guess again:"
+#if smaller
+else
+  TRIES=$(( $TRIES + 1 ))
+  echo "It's lower than that, guess again:"
 fi
 
